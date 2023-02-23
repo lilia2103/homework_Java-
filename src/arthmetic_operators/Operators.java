f49a43ea3136ebedc07f23997ea77bc2665d71f7
lilia200321 <lilia.azaryan@gmail.conm>
2023-02-24 00:09:00 +0400

    test

---
+++ src/arthmetic_operators/Operators.java
@@ -0,0 +1,13 @@
+package arthmetic_operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 6;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
