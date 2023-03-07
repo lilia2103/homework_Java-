ee76423fc95c94d1ce3d2eec519e5f6c1c65e98b
lilia200321 <lilia.azaryan@gmail.conm>
2023-03-07 22:00:53 +0400

    eggs  homeworke

---
+++ src/eggsExecice/Eggs.java
@@ -0,0 +1,14 @@
+package eggsExecice;
+
+public class Eggs {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 200;
+
+        int d = (int) Math.sqrt(1+4*c);
+        double n;
+        System.out.println(n =  (-b + d) / 2);
+        System.out.println(n =  (-b - d) / 2);
+    }
+}
